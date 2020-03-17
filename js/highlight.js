993b24c2a8da1f19c0c31aa08e229e4707254bb1
chenhaojing <870290430@qq.com>
2020-03-17 09:18:56 +0800

    Site updated: 2020-03-17 09:18:53

--- js/highlight.js
+++ js/highlight.js
@@ -1,5 +1,10 @@
+
+<head>
 <link href="https://cdn.bootcss.com/highlight.js/8.0/styles/monokai_sublime.min.css" rel="stylesheet">
+<head>
+<body>
 <script src="https://cdn.bootcss.com/highlight.js/8.0/highlight.min.js"></script>
+</body>
 <script>
     hljs.initHighlightingOnLoad();
 </script>
